53ef3cfe1e08a78e26fac1116bc7d9c9e68e9f69
Enrico Giurin <enricogiurin@gmail.com>
2024-09-29 09:16:50 +0200

    deploying on Azure and updated docs

--- src/main/java/org/enricogiurin/poc/sbai/component/AphorismGenerator.java
+++ src/main/java/org/enricogiurin/poc/sbai/component/AphorismGenerator.java
@@ -1,16 +1,18 @@
 package org.enricogiurin.poc.sbai.component;
 
 import java.util.function.Function;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class AphorismGenerator implements Function<String, String> {
 
-  @Autowired
-  @Qualifier("aphorism")
-  private String aphorism;
+  private final String aphorism;
+
+  public AphorismGenerator(@Qualifier("aphorism") String aphorism) {
+    this.aphorism = aphorism;
+  }
 
   @Override
   public String apply(String s) {
